fb8f6a577d3c6d2689c765ad9a46ec3ca8d927a9
Xurxo Méndez Pérez <sonxurxo@gmail.com>
2013-12-25 14:42:35 +0100

    Users

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :surname, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Api::Application.config.secret_key_base = 'f51a1cb8cf04ba8a0eaf7943ec0393dc4af19d927da05546f55ee5fbf02b1ef5e329c6b86feaf58a60a63e23829a06d52ad82b70ea75b46cd70af8801af8356e'
